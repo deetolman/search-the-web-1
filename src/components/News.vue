b99b97b67bd9a3a9dc9d73fe563216a377092610
Dyanna Tolman <dyannast@gmail.com>
2018-12-03 18:31:14 -0800

    search is kinda working but not refreshing correctly

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,6 +2,7 @@
     <section>
         <h2>News</h2>
         <div>
+            <NewsSearch :onSearch="handleSearch" :search="search"/>
             <ul>
                 <NewsItem v-for="(newsItem, i) in news"
                     :key="i"
@@ -15,22 +16,40 @@
 <script>
 import api from '../services/api.js';
 import NewsItem from './NewsItem';
+import NewsSearch from './NewsSearch';
 
 export default {
     data() {
         return {
             news: null,
+            search: decodeURIComponent(this.$route.query.search),
         };
     },
     components: {
-        NewsItem
+        NewsItem,
+        NewsSearch
     },
     created() {
         this.searchNews();
     },
+    watch: {
+        $route(newRoute, oldRoute) {
+            const newSearch = newRoute.query.search;
+            const oldSearch = oldRoute.query.search;
+            // let newPage = newRoute.query.page;
+            // const oldPage = oldRoute.query.page;
+            if(newSearch === oldSearch) return;
+            if(newSearch !== oldSearch) {
+                // newPage = 1;
+            }
+        }
+    },
     methods: {
+        handleSearch(search) {
+            this.search = search || '';
+        },
         searchNews() {
-            api.getNews()
+            api.getNews(this.search)
                 .then(response => {
                     console.log('response', response.articles);
                     this.news = response.articles;
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export default {
-    getNews() {
-        return fetch('https://newsapi.org/v2/everything?q=apple&apiKey=0dfabefdb08a44cb8866e5824c68c6d1')
+    getNews(searchTerm = '') {
+        return fetch(`https://newsapi.org/v2/everything?q=${encodeURIComponent(searchTerm)}&apiKey=0dfabefdb08a44cb8866e5824c68c6d1`)
             .then(response => response.json());
     }
 };
